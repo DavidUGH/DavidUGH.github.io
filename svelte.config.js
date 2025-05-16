4e02754ca388cfc8117be0bfac3b11d218e85733
David Uziel <daviduziel.gh@gmail.com>
2025-05-16 13:10:02 -0600

    no jekyll

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
+//TODO: tryna make svelte work on this static web
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { 
+
+	kit: {
 		adapter: adapter(),
 		paths: {
-			base: dev ? '' : 'DavidUGH.github.io',
+			base: process.env.NODE_ENV === 'production' ? '/DavidUGH.github.io' : '',
 		}
 	}
 };
