1691e612973282903d38c47935572f4c96e832cd
David Uziel <daviduziel.gh@gmail.com>
2025-05-16 13:43:18 -0600

    Built skills commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,10 +6,10 @@
 
 <div class="flex bg-amber-50 min-h-screen p-4 justify-center items-start md:items-center">
 	<div class="flex flex-row bg-white p-5 max-h-300 shadow-2xl">
-		<div class="flex flex-col p-10 gap-10">
+		<div class="flex flex-col p-10 gap-10 max-w-100">
 			<img class="rounded-full w-80 align-middle justify-center"
 			src="david.jpeg" alt="">
-			<div class="flex flex-col gap-2">
+			<div class="flex flex-col gap-2 ">
 				<p class="text-4xl text-left font-bold">CONTACT</p>
 				<p class="flex items-center text-2xl text-left gap-2">
 					<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 shrink-0" viewBox="0 -960 960 960">
