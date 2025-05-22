9666af560a564b117e004fcb58638e4a72bb6de5
David Uziel <daviduziel.gh@gmail.com>
2025-05-22 14:10:38 -0600

    fix

--- src/lib/components/link-card.svelte
+++ src/lib/components/link-card.svelte
@@ -7,13 +7,8 @@
 		 <p class="text-xl font-bold sm:text-2xl">{name}</p>
 		 {@render children?.()}
 	 </div>
-	 {#if date}
 		 <i>{date}</i>
-	 {:else}
-		 <i>In development</i>
-	 {/if}
 	 <p class="text-lg sm:text-xl">
-		 SvelteKit + Capacitor, TailwindCSS - Designed and developed a responsive mobile app for
-		 tracking expenses with Google Sheets integration.
+		 {description}
 	 </p>
 </a>
